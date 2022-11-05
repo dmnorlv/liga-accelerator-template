d2f8f4f7bdb4e84e1b73b057cdbbcf6a1620d7a6
dmnorlv <dmnorlv21@gmail.com>
2022-11-05 11:56:11 +0500

    подвал v0.4

--- source/js/accordeon.js
+++ source/js/accordeon.js
@@ -1,9 +1,37 @@
 const headingContainers = document.querySelectorAll('.main-footer__heading-container');
-const buttonsFooters = [];
+const sectionsLists = document.querySelector('.main-footer__sections-wrapper').querySelectorAll('ul');
+const contactsLists = document.querySelector('.main-footer__contacts-wrapper').querySelectorAll('ul');
+const footerButtons = [];
+const footerListsArrays = [];
 
+footerListsArrays.push(sectionsLists);
+footerListsArrays.push(contactsLists);
+
+// скрыть все списки ссылок
+footerListsArrays.forEach((array) => {
+  array.forEach((list) => {
+    list.classList.add('main-footer__list-js');
+  });
+});
+
+// добавить контейнеру с копкой класс при рабочем js
+// + показать кнопку + кнопке добавить слушатель
 headingContainers.forEach((element, i) => {
-  buttonsFooters[i] = element.querySelector('button');
-}
-);
+  element.classList.add('main-footer__heading-container-js');
+  footerButtons[i] = element.querySelector('button');
 
-console.log(buttonsFooters);
+  if (footerButtons[i]) {
+    footerButtons[i].classList.remove('main-footer__button-no-js');
+  }
+});
+
+footerButtons.forEach((button, i) => {
+  button.addEventListener('click', () => {
+    button.classList.toggle('main-footer__button-js');
+    const lists = footerListsArrays[i];
+    lists.forEach((list) => {
+      list.classList.toggle('main-footer__list-js');
+    });
+    headingContainers[i].classList.toggle('main-footer__heading-container-js');
+  });
+});
---
+++ source/js/modules/accordeon/accordeon.js
@@ -0,0 +1,9 @@
+const headingContainers = document.querySelectorAll('.main-footer__heading-container');
+const buttonsFooter = [];
+
+headingContainers.forEach((element) => {
+  buttonsFooter[i] = element.querySelector('button');
+  if (buttonsFooter[i]) {
+    buttonsFooter[i].classList.add()
+  }
+});
