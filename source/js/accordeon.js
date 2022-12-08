3bcf6097026eb8f3d3b5d9fff82ac2c026734593
dmnorlv <dmnorlv21@gmail.com>
2022-12-08 20:52:53 +0500

    устранил ошибки в консоли

--- source/js/accordeon.js
+++ source/js/accordeon.js
@@ -1,26 +1,22 @@
 const initAccordeon = () => {
-  const headingContainers = document.querySelectorAll('.main-footer__heading-container');
-  const sectionsLists = document.querySelector('.main-footer__sections-wrapper').querySelectorAll('ul');
-  const contactsLists = document.querySelector('.main-footer__contacts-wrapper').querySelectorAll('ul');
-  const footerButtons = [];
-  const footerListsArrays = [];
+  if (document.querySelectorAll('.main-footer__heading-container') && document.querySelector('.main-footer__sections-wrapper') &&
+    document.querySelector('.main-footer__contacts-wrapper')) {
+    const headingContainers = document.querySelectorAll('.main-footer__heading-container');
+    const sectionsLists = document.querySelector('.main-footer__sections-wrapper').querySelectorAll('ul');
+    const contactsLists = document.querySelector('.main-footer__contacts-wrapper').querySelectorAll('ul');
+    const footerButtons = [];
+    const footerListsArrays = [];
 
-  if (sectionsLists) {
+
     footerListsArrays.push(sectionsLists);
-  }
-
-  if (contactsLists) {
     footerListsArrays.push(contactsLists);
-  }
 
-  // скрыть все списки ссылок
-  footerListsArrays.forEach((array) => {
-    array.forEach((list) => {
-      list.classList.add('main-footer__list-js');
+    footerListsArrays.forEach((array) => {
+      array.forEach((list) => {
+        list.classList.add('main-footer__list-js');
+      });
     });
-  });
 
-  if (headingContainers) {
     headingContainers.forEach((element, i) => {
       element.classList.add('main-footer__heading-container-js');
       footerButtons[i] = element.querySelector('button');
@@ -29,33 +25,33 @@ const initAccordeon = () => {
         footerButtons[i].classList.remove('main-footer__button-no-js');
       }
     });
-  }
 
-  if (footerButtons) {
-    footerButtons.forEach((button, i) => {
-      button.addEventListener('click', (evt) => {
-        button.classList.toggle('main-footer__button-js');
-        const lists = footerListsArrays[i];
-        lists.forEach((list) => {
-          list.classList.toggle('main-footer__list-js');
+    if (footerButtons) {
+      footerButtons.forEach((button, i) => {
+        button.addEventListener('click', (evt) => {
+          button.classList.toggle('main-footer__button-js');
+          const lists = footerListsArrays[i];
+          lists.forEach((list) => {
+            list.classList.toggle('main-footer__list-js');
+          });
+          headingContainers[i].classList.toggle('main-footer__heading-container-js');
+
+          if (evt.target.classList.contains('main-footer__button-sections')) {
+            footerButtons[1].classList.remove('main-footer__button-js');
+            contactsLists.forEach((list) => {
+              list.classList.add('main-footer__list-js');
+            });
+            headingContainers[1].classList.add('main-footer__heading-container-js');
+          } else {
+            footerButtons[0].classList.remove('main-footer__button-js');
+            sectionsLists.forEach((list) => {
+              list.classList.add('main-footer__list-js');
+            });
+            headingContainers[0].classList.add('main-footer__heading-container-js');
+          }
         });
-        headingContainers[i].classList.toggle('main-footer__heading-container-js');
-
-        if (evt.target.classList.contains('main-footer__button-sections')) {
-          footerButtons[1].classList.remove('main-footer__button-js');
-          contactsLists.forEach((list) => {
-            list.classList.add('main-footer__list-js');
-          });
-          headingContainers[1].classList.add('main-footer__heading-container-js');
-        } else {
-          footerButtons[0].classList.remove('main-footer__button-js');
-          sectionsLists.forEach((list) => {
-            list.classList.add('main-footer__list-js');
-          });
-          headingContainers[0].classList.add('main-footer__heading-container-js');
-        }
       });
-    });
+    }
   }
 };
 
